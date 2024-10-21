1f66a560b323404253f29f9c231680a01b7ac8bb
Abhishek Gaurav <abhishekgauravji@icloud.com>
2024-10-21 20:40:20 +0530

    added structural directive code

--- angular18/src/app/app.component.ts
+++ angular18/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddEmployeeComponent } from "./components/add-employee/add-employee.component";
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
+import { StructuralDirComponent } from './components/structural-dir/structural-dir.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DataBindingComponent],
+  imports: [RouterOutlet,AddEmployeeComponent, DataBindingComponent, StructuralDirComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular18/src/app/components/structural-dir/structural-dir.component.ts
+++ angular18/src/app/components/structural-dir/structural-dir.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-structural-dir',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './structural-dir.component.html',
   styleUrl: './structural-dir.component.css'
 })
@@ -13,8 +14,22 @@ export class StructuralDirComponent {
   isDiv1Visible: boolean = false;
   isDiv2Visible: boolean = false;
 
+  num1:string = ''
+  num2:string = ''
+  selectedCountry:string = ''
+
+  cityArray:Array<string> = ['Kolkata', 'Pune','Delhi','Patna']
+
   showDiv1(){
     this.isDiv1Visible = true
+  }
+
+  hideDiv1(){
+    this.isDiv1Visible = false
+  }
+
+  toggleDiv2(){
+    this.isDiv2Visible = !this.isDiv2Visible;
   }
 
 }
